b35c91809acafcaa5f7b9e8c5f4dc436a0f2a1fb
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-03-01 09:02:42 -0700

    prefix trie

---
+++ trees/trie/prefixtrie.js
@@ -0,0 +1,48 @@
+class TrieNode {
+  constructor() {
+    this.characters = []
+    this.wordEnd = false
+  }
+}
+
+class Trie {
+  constructor() {
+    this.trieNode = new TrieNode()
+  }
+
+  insert(word) {
+    let trieLevel = this.trieNode
+
+    for (const c of word) {
+      if (!trieLevel.characters[c]) trieLevel.characters[c] = new TrieNode()
+
+      trieLevel = trieLevel.characters[c]
+    }
+
+    trieLevel.wordEnd = true
+  }
+
+  search(word) {
+    let trieLevel = this.trieNode
+
+    for (const c of word) {
+      if (!trieLevel.characters[c]) return false
+
+      trieLevel = trieLevel.characters[c]
+    }
+
+    return trieLevel.wordEnd
+  }
+
+  startsWith(prefix) {
+    let trieLevel = this.trieNode
+
+    for (const c of prefix) {
+      if (!trieLevel.characters[c]) return false
+
+      trieLevel = trieLevel.characters[c]
+    }
+
+    return true
+  }
+}
