095d867c898e6ca517f38b31c7cdee06aca4f2ae
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-03-05 17:45:16 -0600

    dynamic youtube practice

--- dynamic.js
+++ dynamic.js
@@ -1,72 +1,72 @@
-const combinationSumList = function (target, candidates, memo = new Map()) {
-  if (memo.has(target)) return memo.get(target)
-  if (target === 0) return []
-  if (target < 0) return null
-
-  for (const num of candidates) {
-    const remainder = target - num
-    const result = combinationSum(remainder, candidates, memo)
-
-    if (result !== null) {
-      memo.set(target, [...result, num])
-      return [...result, num]
-    }
-  }
-
-  memo.set(target, null)
-  return null
-}
-
-// const combinationSumTF = function (target, candidates, memo = new Map()) {
+// const combinationSumList = function (target, candidates, memo = new Map()) {
 //   if (memo.has(target)) return memo.get(target)
-//   if (target === 0) return true
-//   if (target < 0) return false
+//   if (target === 0) return []
+//   if (target < 0) return null
 
-//   for (let num of candidates) {
+//   for (const num of candidates) {
 //     const remainder = target - num
+//     const result = combinationSum(remainder, candidates, memo)
 
-//     if (combinationSumTF(remainder, candidates, memo)) {
-//       memo.set(target, true)
-//       return true
+//     if (result !== null) {
+//       memo.set(target, [...result, num])
+//       return [...result, num]
 //     }
 //   }
-//   memo.set(target, false)
-//   return false
+
+//   memo.set(target, null)
+//   return null
 // }
 
-// console.log(combinationSum(7, [2, 3]))
-// console.log(combinationSum(7, [5, 3, 4, 7]))
-// console.log(combinationSum(7, [2, 4]))
-// console.log(combinationSum(8, [2, 3, 5]))
-// console.log(combinationSum(300, [7, 14]))
+const combinationSumTF = function (target, candidates, memo = new Map()) {
+  if (memo.has(target)) return memo.get(target)
+  if (target === 0) return true
+  if (target < 0) return false
 
-const bestSum = (targetSum, numbers, memo = new Map()) => {
-  if (memo.has(targetSum)) return memo.get(targetSum)
-  if (targetSum === 0) return []
-  if (targetSum < 0) return null
+  for (let num of candidates) {
+    const remainder = target - num
 
-  let shortestCombination = null
-
-  for (const num of numbers) {
-    const remainder = targetSum - num
-    const result = bestSum(remainder, numbers, memo)
-
-    if (result !== null) {
-      const combination = [...result, num]
-
-      if (
-        shortestCombination === null ||
-        combination.length < shortestCombination.length
-      )
-        shortestCombination = combination
+    if (combinationSumTF(remainder, candidates, memo)) {
+      memo.set(target, true)
+      return true
     }
   }
-
-  memo.set(targetSum, shortestCombination)
-  return shortestCombination
+  memo.set(target, false)
+  return false
 }
 
-console.log(bestSum(7, [5, 3, 4, 7]))
-console.log(bestSum(8, [2, 3, 5]))
-console.log(bestSum(7, [1, 4, 5]))
-console.log(bestSum(100, [1, 2, 5, 25]))
+console.log(combinationSumTF(7, [2, 3]))
+console.log(combinationSumTF(7, [5, 3, 4, 7]))
+console.log(combinationSumTF(7, [2, 4]))
+console.log(combinationSumTF(8, [2, 3, 5]))
+console.log(combinationSumTF(300, [7, 14]))
+
+// const bestSum = (targetSum, numbers, memo = new Map()) => {
+//   if (memo.has(targetSum)) return memo.get(targetSum)
+//   if (targetSum === 0) return []
+//   if (targetSum < 0) return null
+
+//   let shortestCombination = null
+
+//   for (const num of numbers) {
+//     const remainder = targetSum - num
+//     const result = bestSum(remainder, numbers, memo)
+
+//     if (result !== null) {
+//       const combination = [...result, num]
+
+//       if (
+//         shortestCombination === null ||
+//         combination.length < shortestCombination.length
+//       )
+//         shortestCombination = combination
+//     }
+//   }
+
+//   memo.set(targetSum, shortestCombination)
+//   return shortestCombination
+// }
+
+// console.log(bestSum(7, [5, 3, 4, 7]))
+// console.log(bestSum(8, [2, 3, 5]))
+// console.log(bestSum(7, [1, 4, 5]))
+// console.log(bestSum(100, [1, 2, 5, 25]))
