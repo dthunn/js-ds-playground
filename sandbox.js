791177a433dd0f537a9ea53420606006bbf1f6eb
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-04-24 14:35:09 -0600

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,19 @@
-var findCheapestPrice = function (n, flights, src, dst, k) {
-  let dist = new Array(n).fill(Infinity)
-  dist[src] = 0
+var bstFromPreorder = function (preorder) {
+  let idx = 0
 
-  for (let i = 0; i <= k; i++) {
-    const temp = [...dist]
+  function helper(lower, upper) {
+    if (idx === preorder.length) return null
 
-    for (const flight of flights) {
-      if (dist[flight[0]] !== Infinity) {
-        temp[flight[1]] = Math.min(temp[flight[1]], dist[flight[0]] + flight[2])
-      }
-    }
+    const val = preorder[idx]
+    if (val < lower || val > upper) return null
+    idx++
 
-    dist = [...temp]
+    const root = new TreeNode(val)
+    root.left = helper(lower, val)
+    root.right = helper(val, upper)
+
+    return root
   }
 
-  return dist[dst] === Infinity ? -1 : dist[dst]
+  return helper(-Infinity, Infinity)
 }
