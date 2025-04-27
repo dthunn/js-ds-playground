068dc30ef3c83dade08ece16f41e19669afa97be
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-04-27 11:49:52 -0600

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,45 +1,36 @@
-const dummyMatrix = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9, 10, 11, 12],
-  [13, 14, 15, 16],
-]
-
-const directions = [
-  [-1, 0], //up
-  [0, 1], //right
-  [1, 0], //down
-  [0, -1], //left
-]
-
-const traversal = function (matrix) {
-  const seen = new Array(matrix.length)
-    .fill(0)
-    .map(() => new Array(matrix[0].length).fill(0))
-
-  const values = []
-  dfs(matrix, 0, 0, seen, values)
-
-  return values
-}
-
-const dfs = function (matrix, row, col, seen, values) {
-  if (
-    row < 0 ||
-    col < 0 ||
-    row >= matrix.length ||
-    col >= matrix[0].length ||
-    seen[row][col]
-  ) {
-    return
-  }
-
-  seen[row][col] = true
-  values.push(matrix[row][col])
-
-  for (const dir of directions) {
-    dfs(matrix, row + dir[0], col + dir[1], seen, values)
+class TrieNode {
+  constructor() {
+    this.characters = {}
+    this.wordEnd = false
   }
 }
 
-console.log(traversal(dummyMatrix))
+class Trie {
+  constructor() {
+    this.trieNode = new TrieNode()
+  }
+
+  insert(word) {
+    let trieLevel = this.trieNode
+
+    for (const c of word) {
+      if (!trieLevel.characters[c]) trieLevel.characters[c] = new TrieNode()
+
+      trieLevel = trieLevel.characters[c]
+    }
+
+    trieLevel.wordEnd = true
+  }
+
+  search(word) {
+    let trieLevel = this.trieNode
+
+    for (const c of word) {
+      if (!trieLevel.characters[c]) return false
+
+      trieLevel = trieLevel.characters[c]
+    }
+
+    return trieLevel.wordEnd
+  }
+}
