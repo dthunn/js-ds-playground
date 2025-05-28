c9ef360205d1be26f3e649eb60d782a9bcaa5159
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-05-28 12:07:34 -0600

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,34 @@
-var solution = function (isBadVersion) {
-  /**
-   * @param {integer} n Total versions
-   * @return {integer} The first bad version
-   */
-  return function (n) {
-    let left = 1
-    let right = n
+var findMedianSortedArrays = function (nums1, nums2) {
+  if (nums1.length > nums2.length) [nums1, nums2] = [nums2, nums1]
 
-    while (left < right) {
-      const mid = left + Math.floor((right - left) / 2)
+  const m = nums1.length,
+    n = nums2.length
+  let left = 0,
+    right = m
 
-      if (isBadVersion(mid)) {
-        right = mid
+  while (left <= right) {
+    const pA = Math.floor((left + right) / 2)
+    const pB = Math.floor((m + n + 1) / 2 - pA)
+
+    const maxLeftA = pA === 0 ? -Infinity : nums1[pA - 1]
+    const minRightA = pA === m ? Infinity : nums1[pA]
+    const maxLeftB = pB === 0 ? -Infinity : nums2[pB - 1]
+    const minRightB = pB === n ? Infinity : nums2[pB]
+
+    if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+      if ((m + n) % 2 === 0) {
+        return (
+          (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2
+        )
       } else {
-        left = mid + 1
+        return Math.max(maxLeftA, maxLeftB)
       }
+    } else if (maxLeftA > minRightB) {
+      right = pA - 1
+    } else {
+      left = pA + 1
     }
-
-    return left
   }
+
+  return -1
 }
