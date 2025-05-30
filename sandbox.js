53b2eb9b859496f029ad144978cf2adb6282cf8e
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-05-30 07:26:24 -0600

    topo

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,28 @@
-function kthSmallest(root, k) {
-  const stack = []
+var canFinish = function (numCourses, prerequisites) {
+  const inDegree = new Array(numCourses).fill(0)
+  const adjList = inDegree.map(() => [])
 
-  while (true) {
-    // Traverse the left subtree
-    while (root) {
-      stack.push(root)
-      root = root.left
-    }
-
-    // Process the node
-    root = stack.pop()
-    k--
-    if (k === 0) {
-      return root.val
-    }
-
-    // Traverse the right subtree
-    root = root.right
+  for (const [course, prereq] of prerequisites) {
+    inDegree[course]++
+    adjList[prereq].push(course)
   }
+
+  const stack = []
+  let count = 0
+
+  for (let i = 0; i < inDegree.length; i++) {
+    if (inDegree[i] === 0) stack.push(i)
+  }
+
+  while (stack.length) {
+    const current = stack.pop()
+    count++
+
+    for (const next of adjList[current]) {
+      inDegree[next]--
+      if (inDegree[next] === 0) stack.push(next)
+    }
+  }
+
+  return count === numCourses
 }
