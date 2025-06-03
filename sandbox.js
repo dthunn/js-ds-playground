a13da6074dafe4d68bbdf0ef145eb14bb71d410a
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-06-03 06:55:26 -0600

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,17 @@
-const insertIntoBST = function (root, val) {
-  if (!root) return new TreeNode(val)
+var fizzBuzz = function (n) {
+  const ans = []
 
-  if (val > root.val) {
-    root.right = insertIntoBST(root.right, val)
-  } else {
-    root.left = insertIntoBST(root.left, val)
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      ans.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      ans.push('Fizz')
+    } else if (i % 5 === 0) {
+      ans.push('Buzz')
+    } else {
+      ans.push(i.toString())
+    }
   }
 
-  return root
+  return ans
 }
