9f889eccc9959f3ed0f68acb6de5dc97ad20536a
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-07-02 09:00:01 -0600

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,45 @@
-function binarySearch(array, target) {
-  let left = 0
-  let right = array.length - 1
+const dummyMatrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+]
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
-    const foundVal = array[mid]
+const directions = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+]
 
-    if (foundVal === target) {
-      return mid
-    } else if (target < foundVal) {
-      right = mid - 1
-    } else if (target > foundVal) {
-      left = mid + 1
+const bfs = function (matrix) {
+  const seen = new Array(matrix.length)
+    .fill(0)
+    .map(() => new Array(matrix[0].length).fill(0))
+
+  const values = []
+  const queue = [[0, 0]]
+
+  while (queue.length) {
+    const [row, col] = queue.shift()
+
+    if (
+      row >= 0 &&
+      col >= 0 &&
+      row < matrix.length &&
+      col < matrix[0].length &&
+      !seen[row][col]
+    ) {
+      seen[row][col] = true
+      values.push(matrix[row][col])
+
+      for (const [dr, dc] of directions) {
+        queue.push([row + dr, col + dc])
+      }
     }
   }
 
-  return -1
+  return values
 }
+
+console.log(bfs(dummyMatrix))
