129d74ccb344442f9c840df2d557909319ea0463
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-07-24 07:07:35 -0600

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,40 +1,12 @@
-const shortestPathBinaryMatrix = function (grid) {
-  if (grid[0][0] !== 0 || grid[grid.length - 1][grid[0].length - 1] !== 0)
-    return -1
+const removeDuplicates = function (nums) {
+  let insertIdx = 1
 
-  const n = grid.length
-  const visited = new Array(n).fill(0).map(() => new Array(n).fill(false))
-  const queue = [[0, 0]]
-  visited[0][0] = true
-  let currentDistance = 1
-
-  while (queue.length) {
-    const nodesSize = queue.length
-
-    for (let i = 0; i < nodesSize; i++) {
-      const [row, col] = queue.shift()
-      if (row === n - 1 && col === n - 1) return currentDistance
-
-      for (const [dr, dc] of directions) {
-        const newRow = row + dr
-        const newCol = col + dc
-
-        if (
-          newRow >= 0 &&
-          newCol >= 0 &&
-          newRow < n &&
-          newCol < n &&
-          grid[newRow][newCol] === 0 &&
-          !visited[newRow][newCol]
-        ) {
-          visited[newRow][newCol] = true
-          queue.push([newRow, newCol])
-        }
-      }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] !== nums[i]) {
+      nums[insertIdx] = nums[i]
+      insertIdx++
     }
-
-    currentDistance++
   }
 
-  return -1
+  return insertIdx
 }
