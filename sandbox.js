7d908ad85e79c35869f9e3a734eff5514561d83d
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-08-16 07:28:15 -0500

    sandbox

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,45 @@
-var lengthOfLongestSubstring = function (s) {
-  let charToNextIndex = new Map()
-  let maxLen = 0
-  let left = 0
+const directions = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+]
 
-  for (let right = 0; right < s.length; right++) {
-    if (charToNextIndex.has(s[right])) {
-      left = Math.max(charToNextIndex.get(s[right]), left)
+const numIslands = function (matrix) {
+  if (!matrix.length) return 0
+  let count = 0
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[0].length; col++) {
+      if (matrix[row][col] === '1') {
+        count++
+        const queue = [[row, col]]
+        matrix[row][col] = '0'
+
+        while (queue.length) {
+          const [currentRow, currentCol] = queue.shift()
+
+          for (const [dr, dc] of directions) {
+            const nextRow = currentRow + dr
+            const nextCol = currentCol + dc
+
+            if (
+              nextRow < 0 ||
+              nextCol < 0 ||
+              nextRow >= matrix.length ||
+              nextCol >= matrix[0].length
+            )
+              continue
+
+            if (matrix[nextRow][nextCol] === '1') {
+              queue.push([nextRow, nextCol])
+              matrix[nextRow][nextCol] = '0'
+            }
+          }
+        }
+      }
     }
-
-    maxLen = Math.max(maxLen, right - left + 1)
-    charToNextIndex.set(s[right], right + 1)
   }
 
-  return maxLen
+  return count
 }
